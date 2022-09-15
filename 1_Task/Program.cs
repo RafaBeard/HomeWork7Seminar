1e4ebfac0d0355ed6d3f1a69447bd220b3c0ee5f
TRI <rafael.tagirzyanov@yandex.ru>
2022-09-15 16:34:47 +0300

    Первая задача

---
+++ 1_Task/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите кол-во строк: ");
+int linesVolume = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columnsVolume = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVolume, columnsVolume];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
